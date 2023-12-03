17d4401ee00f735afaa9ab205f2216776c6f4182
Mohamed Meabed <mo.meabed@gmail.com>
2023-12-03 02:34:11 -0500

    chore: update

--- test/httpsnippetGenerator.test.js
+++ test/httpsnippetGenerator.test.js
@@ -12,12 +12,13 @@ const sampleData = {
 };
 
 describe('httpsnippetGenerator tests', () => {
-    it('should return code snippet', () => {
+    it('should return code snippet', async() => {
         const testData = Object.assign({}, sampleData);
         const expected = 'curl --request GET \\\n  --url http://sample.com/books';
 
-        const result = httpsnippetGenerator.generate('shell', 'curl', testData);
+        const result = await httpsnippetGenerator.generate('shell', 'curl', testData);
 
+        console.assert({result, expected})
         assert.deepEqual(result, expected);
     });
 });
