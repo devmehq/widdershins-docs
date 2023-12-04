464ade2fbaf0075f27a68a71a230c0baedc84390
Mohamed Meabed <mo.meabed@gmail.com>
2023-12-03 21:32:35 -0500

    chore: update

--- test/httpsnippetGenerator.test.js
+++ test/httpsnippetGenerator.test.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const httpsnippetGenerator = require('../lib/httpsnippetGenerator');
+const assert = require("assert");
+const httpsnippetGenerator = require("../lib/httpsnippetGenerator");
 
 const sampleData = {
-    methodUpper: 'GET',
-    baseUrl: 'http://sample.com',
-    requiredUriExample: '/books',
-    queryParameters: [],
-    allHeaders: []
+  methodUpper: "GET",
+  baseUrl: "http://sample.com",
+  requiredUriExample: "/books",
+  queryParameters: [],
+  allHeaders: [],
 };
 
-describe('httpsnippetGenerator tests', () => {
-    it('should return code snippet', async() => {
-        const testData = Object.assign({}, sampleData);
-        const expected = 'curl --request GET \\\n  --url http://sample.com/books';
+describe("httpsnippetGenerator tests", () => {
+  it("should return code snippet", async () => {
+    const testData = Object.assign({}, sampleData);
+    const expected = "curl --request GET \\\n  --url http://sample.com/books";
 
-        const result = await httpsnippetGenerator.generate('shell', 'curl', testData);
+    const result = await httpsnippetGenerator.generate("shell", "curl", testData);
 
-        console.assert({result, expected})
-        assert.deepEqual(result, expected);
-    });
+    console.assert({ result, expected });
+    assert.deepEqual(result, expected);
+  });
 });
