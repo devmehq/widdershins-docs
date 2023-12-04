7c85bcbf226d677d84ed651c96468f758d9b63d3
Mohamed Meabed <mo.meabed@gmail.com>
2023-12-03 21:57:04 -0500

    chore: update

--- test/codeSamples.test.js
+++ test/codeSamples.test.js
@@ -58,14 +58,14 @@ describe("getCodeSamples tests", () => {
   let result, testData;
 
   describe("when x-code-samples are included", () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       testData = clone(sampleData);
       testData.operation["x-code-samples"] = clone(xCodeSamples);
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
     });
 
-    it("should generate code samples from x-code-samples content", () => {
+    it("should generate code samples from x-code-samples content", async () => {
       const expected = `\`\`\`csharp\n${xCodeSamples[0].source}\n\`\`\`\n\n` + `\`\`\`php\n${xCodeSamples[1].source}\n\`\`\`\n\n` + `\`\`\`sample\n${xCodeSamples[2].source}\n\`\`\`\n\n`;
 
       assert.equal(result, expected);
@@ -79,19 +79,19 @@ describe("getCodeSamples tests", () => {
       assert.deepEqual(testData.header.language_tabs, expectedLanguageTabs);
     });
 
-    it("should return empty string if no code samples can be generated", () => {
+    it("should return empty string if no code samples can be generated", async () => {
       testData.operation["x-code-samples"] = [];
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.equal(result, "");
     });
   });
 
-  describe("when x-code-samples are not available", () => {
+  describe("when x-code-samples are not available", async () => {
     let originalHttpsnippetGenerator;
 
-    beforeEach(() => {
+    beforeEach(async () => {
       testData = clone(sampleData);
       testData.templates = {
         code_csharp: () => `csharp-sample`,
@@ -108,49 +108,49 @@ describe("getCodeSamples tests", () => {
       httpsnippetGenerator.generate = originalHttpsnippetGenerator;
     });
 
-    it("should generate code samples using template files by default", () => {
+    it("should generate code samples using template files by default", async () => {
       const expected = `\`\`\`csharp\ncsharp-sample\n\`\`\`\n\n` + `\`\`\`php\nphp-sample\n\`\`\`\n\n`;
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.deepEqual(result, expected);
     });
 
-    it("should use httpsnippet to generate code samples if it is active", () => {
+    it("should use httpsnippet to generate code samples if it is active", async () => {
       const testData = clone(sampleData);
       testData.options = { httpsnippet: true };
       const expected = `\`\`\`csharp\nhttpsnippet-csharp--sample\n\`\`\`\n\n` + `\`\`\`php\nhttpsnippet-php--sample\n\`\`\`\n\n`;
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.deepEqual(result, expected);
     });
 
-    it("should support specifying language custom target name", () => {
+    it("should support specifying language custom target name", async () => {
       testData.header.language_tabs[0] = "javascript--nodejs";
       const expected = `\`\`\`javascript--nodejs\nnodejs-sample\n\`\`\`\n\n` + `\`\`\`php\nphp-sample\n\`\`\`\n\n`;
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.deepEqual(result, expected);
     });
 
-    it("should support specifying language custom client name", () => {
+    it("should support specifying language custom client name", async () => {
       const testData = clone(sampleData);
       testData.options = { httpsnippet: true };
       testData.header.language_tabs[0] = "javascript--nodejs";
       testData.options.language_clients = [{ "javascript--nodejs": "request" }];
       const expected = `\`\`\`javascript--nodejs\nhttpsnippet-nodejs-request-sample\n\`\`\`\n\n` + `\`\`\`php\nhttpsnippet-php--sample\n\`\`\`\n\n`;
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.deepEqual(result, expected);
     });
 
-    it("should return empty string if no code samples can be generated", () => {
+    it("should return empty string if no code samples can be generated", async () => {
       testData.header.language_tabs = ["unknown"];
 
-      result = common.getCodeSamples(testData);
+      result = await common.getCodeSamples(testData);
 
       assert.deepEqual(result, "");
     });
